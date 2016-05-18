5c58c17bfd8ee551b2bbccab04d7b799d5e5541f
annadeu3 <annadeu3@gmail.com>
2016-05-18 17:44:29 -0400

    starting homepage

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var User = require('./models/user');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 
 var app = express();
 
@@ -19,6 +21,10 @@ mongoose.connect('mongodb://annadeu3:abc123@ds025772.mlab.com:25772/ecommerce', 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+
 
 app.post('/create-user', function (request, response, next) {
 	var user = new User();
@@ -29,10 +35,15 @@ app.post('/create-user', function (request, response, next) {
 
 	user.save(function(err) {
 		if (err) return next(err);
-
 		response.json("Successfully created a new user");
 	});
 });
+
+
+app.get('/', function(request, response) {
+	response.render('home');
+});
+
 
 app.listen(3000, function (err) {
 	if (err) throw err;
