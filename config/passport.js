eacf238149a3fa2aea63333b7c8350fa86b7f74a
annadeu3 <annadeu3@gmail.com>
2016-05-19 14:07:04 -0400

    passport file almost complete

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,35 @@ passport.deserializeUser(function(id, done) {
 
 //Middleware
 
+passport.use('local-login', new LocalStrategy( {
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true
+}, function(request, email, password, done) {
+	User.findOne({ email: email}, function(err, user) {
+		if (err) return done(err);
+
+		if (!user) {
+			return done(null, false, request.flash('loginMessage', 'No user has been found'));
+		}
+
+		if (!user.comparePassword(password)) {
+			return done(null, false, request.flash('loginMessage', 'OOPS! Wrong Password'));
+		}
+
+		return done(null, user);
+	});
+}));
+
 
 
 //custom function to validate
+exports.isAuthenticated = function (request, response, next) {
+	if (request.isAuthenticated()) {
+		return next();
+	}
+	response.redirect('/login');
+};
+
+
 
